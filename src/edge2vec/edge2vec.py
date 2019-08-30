cd48b59b008ce084c8615db32e08dcd2e0741814
lingling93 <763704059@qq.com>
2019-08-30 10:10:05 +0200

    Remove list on walks

--- src/edge2vec/edge2vec.py
+++ src/edge2vec/edge2vec.py
@@ -82,6 +82,7 @@ def get_walks(graph, num_walks, walk_length, matrix, p, q, use_multiprocessing: 
             logger.warning(f'Use multiprocessing on {cpu_count()} cores')
             chunksize=len(shuffled_nodes)//cpu_count()
             walks = p.map(partial_get_walk, shuffled_nodes,chunksize=chunksize)
+            print(walks)
     else:
         walks = []
         for node in nodes:
@@ -210,7 +211,7 @@ def train(
         q,
     )
     return Word2Vec(
-        list(walks),
+        walks,
         size=size or 100,
         window=window or 5,
     )
